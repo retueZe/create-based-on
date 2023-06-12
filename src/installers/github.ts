07c019323b71822bcb1783ff95b4a446d73c3d4b
retueZe <84510709+retueZe@users.noreply.github.com>
2023-06-12 18:08:46 +0300

    Add implicit template spec pre- & postfix handler

--- src/installers/github.ts
+++ src/installers/github.ts
@@ -5,19 +5,39 @@ import chalk from 'chalk'
 import { extractTarball } from '../extractTarball.js'
 
 export async function githubInstaller(archievePath: string, directory: string): Promise<ITemplate> {
-    const response = await(await verboseFetch(`https://api.github.com/repos/${archievePath}`)).json()
-
-    if ('message' in response) throw new Error('GitHub API error: ' + response.message)
-
+    const [response, path] = await findRepo(archievePath)
     const defaultBranch = response.default_branch
 
     console.log(chalk.gray`Template was found at default branch {cyan ${defaultBranch}}.`)
 
-    const tarResponse = await verboseFetch(`https://github.com/${archievePath}/tarball/${defaultBranch}`)
+    const tarResponse = await verboseFetch(`https://github.com/${path}/tarball/${defaultBranch}`)
 
     if (!tarResponse.ok) throw new Error('GitHub API error: ' + (await tarResponse.json()).message)
 
     await extractTarball(tarResponse.body!, directory)
 
     return await fileInstaller(directory, directory)
+}
+async function findRepo(path: string): Promise<[any, string]> {
+    if (!path.includes('/')) path = 'project-factory-templates/' + path
+
+    const paths = [
+        path,
+        path + '-project-template'
+    ]
+    let response: Response | null = null
+
+    for (const path of paths) {
+        response = await verboseFetch(`https://api.github.com/repos/${path}`)
+
+        if (Math.abs(response.status - 404) < 0.5) continue
+
+        const body = await response.json()
+
+        if (!response.ok) throw new Error('GitHub API error: ' + body.message)
+
+        return [body, path]
+    }
+
+    return [await response!.json(), path]
 }
